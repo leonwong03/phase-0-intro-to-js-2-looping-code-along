0771c372b4df10a9144f326cb5ccb3d0918839f0
leonwong03 <108467768+leonwong03@users.noreply.github.com>
2022-08-01 08:23:52 -0400

    commit tree ce50cff40b02b63983af88c9dce02bc947ec326b

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names,event){
+    const newArray = [];
+    for (let i = 0; i < names.length; i++){
+        newArray[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    }
+    return newArray;
+}
+
+function countDown(number){
+    while (number >=0){
+
+        console.log(number);
+        number--;
+
+    }
+}
